37f857265baeabae0b5892bd9bd5a1bb4719bb00
VSNSRUJANA123 <srujanadvsn@gmail.com>
2024-12-03 11:40:40 +0530

    commit

--- src/components/Sidenav.js
+++ src/components/Sidenav.js
@@ -1,5 +1,6 @@
 import { NavLink, useNavigate } from "react-router-dom";
 import "../styles/sidenav.css";
+import { BsReverseLayoutSidebarInsetReverse } from "react-icons/bs";
 import Cookies from "js-cookie";
 const Sidenav = () => {
   const navigate = useNavigate();
@@ -10,47 +11,47 @@ const Sidenav = () => {
   };
   return (
     <div className="side-nav-content">
-      <NavLink className="link" to="/dashboard">
-        <h5>
-          Admin
-          <br /> Dashboard
-        </h5>
-      </NavLink>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          flexDirection: "column",
-          height: "80vh",
-        }}
-      >
-        <ul>
-          <li>
-            <NavLink className="link" to="/employee">
-              Employee
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className="link" to="/supplier">
-              Supplier
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className="link" to="/product">
-              Product
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className="link" to="/order">
-              Order
-            </NavLink>
-          </li>
-          <li>
-            <button className="logOut-btn" onClick={handleLogout}>
-              Logout
-            </button>
-          </li>
-        </ul>
+      <BsReverseLayoutSidebarInsetReverse className="small-icon" />
+      <div className="small-side-nav">
+        <NavLink className="link" to="/dashboard">
+          <h5 className="admin-title">Dashboard</h5>
+        </NavLink>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            flexDirection: "column",
+            height: "80vh",
+          }}
+        >
+          <ul>
+            <li>
+              <NavLink className="link" to="/employee">
+                Employee
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="link" to="/supplier">
+                Supplier
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="link" to="/product">
+                Product
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="link" to="/order">
+                Order
+              </NavLink>
+            </li>
+            <li>
+              <button className="logOut-btn" onClick={handleLogout}>
+                Logout
+              </button>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
